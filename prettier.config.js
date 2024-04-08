e531eadf915043a9f50d9f83bf5df1f22ccdc962
Diogo Viana <diogo.g.viana@gmail.com>
2024-04-08 01:41:30 +0100

    initial project setup

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+// @ts-check
+
+/** @type {import('prettier').Config} */
+const config = {
+	semi: true,
+	singleQuote: false,
+	trailingComma: "all",
+	useTabs: true,
+	tabWidth: 2,
+};
+
+export default config;
