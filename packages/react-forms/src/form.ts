8a987ec4a73da37c4a0e0da4ab5ef54fced7f2fa
Diogo Viana <diogo.g.viana@gmail.com>
2025-05-29 09:08:31 +0100

    add form options to vue and react adapters

--- packages/react-forms/src/form.ts
+++ packages/react-forms/src/form.ts
@@ -1,11 +1,22 @@
-import { BaseFormType, Form, FormOptions, FormResult } from "@deadcow-enterprises/forms-core";
-import { FormEvent, useEffect, useState } from "react";
+import {
+	BaseFormType,
+	Form,
+	FormConfig,
+	FormOptions,
+	FormResult,
+} from '@deadcow-enterprises/forms-core';
+import { FormEvent, useEffect, useState } from 'react';
 
-export function useForm<T extends BaseFormType>(config: FormOptions<T>): FormResult<T, FormEvent> {
-	const [instance] = useState(new Form<T, FormEvent>(config));
-	const [formResult, setFormResult] = useState<FormResult<T, FormEvent>>(instance.currentState);
+export function useForm<T extends BaseFormType>(
+	config: FormOptions<T>,
+	options?: FormConfig,
+): FormResult<T, FormEvent> {
+	const [instance] = useState(new Form<T, FormEvent>(config, options));
+	const [formResult, setFormResult] = useState<FormResult<T, FormEvent>>(
+		instance.currentState,
+	);
 	useEffect(() => {
-		return instance.listen(setFormResult)
+		return instance.listen(setFormResult);
 	}, [instance]);
 	return formResult;
 }
